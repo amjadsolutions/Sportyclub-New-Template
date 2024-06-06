f444d4aef535ce32e4bb69bc3cf8033461d6bfe6
amjadsolutions <amjadjavedse@gmail.com>
2024-06-06 15:32:27 +0400

    add main

---
+++ dist/js/desktop-version/newTemplate8.js
@@ -0,0 +1,27 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [153, 152, 156],
+  {
+    151: function (e, o, t) {
+      "use strict";
+      o.a = {
+        title: "Free Livescore Widget - AiScore Football Livescore",
+        meta: [
+          {
+            name: "description",
+            hid: "description",
+            content:
+              "AiScore Free Livescore Widget. Get the fastest, reliable and accurate Football Livescore widget from us. With in-depth statistics and odds comparison and more.",
+          },
+          {
+            name: "keywords",
+            hid: "keywords",
+            content: "free livescore widget",
+          },
+        ],
+      };
+    },
+    1838: function (e, o, t) {
+      e.exports = t.p + "media/football.dd25db9.mp3";
+    },
+  },
+]);
